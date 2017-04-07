c9c8fd0d3d313fbb23014125064afc4bf983757d
wxt <xueting.wang@acedata.com.cn>
2017-04-07 13:45:01 +0800

    test pull

--- src/first/MyFirst.java
+++ src/first/MyFirst.java
@@ -4,6 +4,10 @@ public class MyFirst {
 
 	public static void main(String[] args) {
 		System.out.println("wxt");
+		System.out.println("wxt");
+		System.out.println("wxt");
+		System.out.println("wxt");
+		System.out.println("wxt");
 	}
 	
 }
