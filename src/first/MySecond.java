d1e28523676ced7cd7cf4f69b214dd61a3a654a9
wxt <xueting.wang@acedata.com.cn>
2017-04-07 13:53:35 +0800

    create MySecond.java

---
+++ src/first/MySecond.java
@@ -0,0 +1,12 @@
+package first;
+
+public class MySecond {
+
+	public static void main(String[] args) {
+		System.out.println("wxt2");
+		System.out.println("wxt2");
+		System.out.println("wxt2");
+		System.out.println("wxt2");
+	}
+	
+}
